93176be13e8f9180b3ab488e1c1026f1dedf4335
cjalvaradoh <cjalvarado@gmail.com>
2024-09-21 23:57:44 -0600

    api

---
+++ Server/Controllers/CharactersController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using APIRM.Shared.Models;
+using Newtonsoft.Json;
+namespace APIRM.Server.Controllers
+{
+    [Route("api/characters")]
+    public class CharactersController : Controller
+    {
+        private HttpClient _httpClient;
+        public CharactersController()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [HttpGet]
+        [Route("all")]
+
+        public async Task<Characters> GetAllCharacters(int page)
+        {
+            try
+            {
+                Characters characters = null;
+                HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync($"https://rickandmortyapi.com/api/character?page={page}");
+                httpResponseMessage.EnsureSuccessStatusCode();
+
+                string responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+
+                characters = JsonConvert.DeserializeObject<Characters>(responseBody);
+
+                return characters;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ Shared/Models/Characters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace APIRM.Shared.Models
+{
+    public class Characters
+    {
+        public Info info { get; set; }
+        public List<Character> results { get; set; }
+    }
+
+    public class Info
+    {
+        public int count { get; set; }
+        public int pages { get; set; }
+        public string next { get; set; }
+        public object prev { get; set; }
+    }
+
+
+}
